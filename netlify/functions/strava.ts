ccbc511851b0d515d890f7c984a844f35c3bb28f
frontend.irish <adrian@frontend.irish>
2026-04-19 11:54:53 +0200

    Revert "chore: log strava types"
    This reverts commit bc041c0be0ba2f32794f907c80edf1a3f2b70f55.

--- netlify/functions/strava.ts
+++ netlify/functions/strava.ts
@@ -73,11 +73,6 @@ export const handler: Handler = async (event) => {
       { Authorization: `Bearer ${tokenData.access_token}` },
     );
 
-    const uniqueTypes = [
-      ...new Set(activities.map((a) => a.sport_type ?? a.type)),
-    ];
-    console.log("Activity count:", activities.length, "Types:", uniqueTypes);
-
     const runs = activities.filter(
       (activity) => activity.sport_type === "Run" || activity.type === "Run",
     );
